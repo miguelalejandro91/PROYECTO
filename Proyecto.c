b9d647ec1e98c2d35ead73830b92dd027357dce5
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-07-10 16:51:39 -0500

    crear el repositorio

---
+++ Proyecto.c
@@ -0,0 +1,7 @@
+/*crea una aplicacion que permita el manejo de datos de una cuenta bancaria. realizar depositos, retiros y consulta
+de saldos, con estos argumentos. construye un programa en donde los estudiantes deben crear un proyecto de programacion utilizando lenguaje C. 
+Una caracteristica importante de la aplicacion es que siempre debe encontrarse en ejecucion, debe permitir el
+ingreso de datos por parte del usuario, de acuerdo con el tema asignado y realizar operaciones de lectura, 
+actualizacion, borrado, busqueda de datos. ademas la aplicacion debe guardar cada uno de los datos en un archivo 
+plano de tipo .CSV (valores separados por comas). cada registro guardado en el archivo plano debe incluir tambien 
+los datos de fecha y hora de creacion del registro*/
