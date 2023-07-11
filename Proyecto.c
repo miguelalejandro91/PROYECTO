9eb8377ef2da628069224c496946f26ffb06e845
miguelalejandro91 <miguel.rodriguez.calderon@udla.edu.ec>
2023-07-10 20:17:52 -0500

    funcion para que de la feche y hora actual

--- Proyecto.c
+++ Proyecto.c
@@ -28,3 +28,12 @@ void guardarRegistro(CuentaBancaria cuenta) {
         printf("Error al abrir el archivo.\n");
         return;
     }
+
+    // Obtener la fecha y hora actual
+    time_t tiempo;
+    struct tm* fecha;
+    char fechaHora[MAXimo];
+
+    tiempo = time(NULL);
+    fecha = localtime(&tiempo);
+    strftime(fechaHora, MAXimo, "%Y-%m-%d %H:%M:%S", fecha);
